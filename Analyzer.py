c7da95903fdc55027b2a144234a9701cb68a4b7a
ShaiHabi <shaihabi@mail.tau.ac.il>
2025-07-24 19:08:00 +0300

    Updating model interaction

--- Analyzer.py
+++ Analyzer.py
@@ -7,13 +7,13 @@ import transformers
 
 def extract_content_safely(file):
     """
-    Extracts the file's content lines securely in order to give it to PHI4 model.
+    Extracts the file's content lines securely in order to give it to gemma31b model.
     Minimizing the risk of executing the file, which may be malicious.
     """
     try:
         with open(file, "r") as f:
             lines = f.readlines()  # List of strings
-            result = ["{}: {}".format(i+1, line.strip()) for i, line in enumerate(lines)]
+            result = ["Line {}: {}".format(i+1, line.strip()) for i, line in enumerate(lines)]
             return result
 
     except Exception as error:
@@ -21,7 +21,7 @@ def extract_content_safely(file):
         failure_message(message)
 
 
-def split_for_model(text_lines, size=6000):
+def split_for_model(text_lines, size=1000):
     """
 
     :param text_lines:
@@ -75,31 +75,33 @@ def build_prompt(text, classification, extension, relevant_data=None):
     }
 
     if classification == "file":
-        prompt = ("You're conducting a static analysis for a {} code file to detect potential vulnerabilities and security flaws."
-                  "The following block is part of the overall code, which was split to small sizes-blocks."
-                  "Read it and carefully analyze this block and identify any potential vulnerabilities and security flaws."
-                  "For each finding, your response should be as follow:"
-                  "Line X: Possible <flaw/vulnerability> due to <give the reason here>.\n"
-                  "Here is the {} code block: {}.".format(extension, extension, text))
+        prompt = ("Conduct a static analysis for a {} code file to detect vulnerabilities and security flaws.\n"
+                  "The following block is a part of the overall code, which was split to small sizes-blocks.\n"
+                  "Read and analyze this block and identify any *meaningful* vulnerabilities and security flaws.\n"
+                  "For each finding,you must write and follow this format:\n"
+                  "'Line <number>: Possible <flaw/vulnerability> \n'.\n"
+                  "Begin each finding in a new row.Do NOT give more explanations, and do not repeat the prompt itself!\n"
+                  "Here is the code:\n {}.".format(extension, text)) # I deleted relevant_data
 
 
 
     else:  # classification == "summarize"
-        prompt = ("You've conducted a static analysis for a {} code file to detect potential vulnerabilities and security flaws."
-                  "The following block is part of your overall findings."
+        prompt = ("Conduct a static analysis for a {} code file to detect potential vulnerabilities and security flaws."
+                  "The following block is a segment of the full code."
+                  "Identify only meaningful issues — avoid speculative or trivial findings (e.g., harmless includes)."
                   "Read it carefully; If you have new conclusions, mention them in the reasoning section below."
                   "Otherwise, just repeat what is already written."
                   "Your response should follow this format, when you have the basic data you need in the block:"
                   "Line X : Possible <flaw/vulnerability> due to <give the reason here> \n."
-                  "Here is the {} code block: {}". format(extension, extension, text))
+                  "Here is the code: {}". format(extension, text))
 
-
+    # return prompt
     user = {"role": "user", "content": prompt}
     message = [system, user]
     return message
 
 
-def using_model(content_lines, extension, path_to_phi4):
+def using_model(content_lines, extension, path_to_gemma3):
     """
 
     """
@@ -109,30 +111,41 @@ def using_model(content_lines, extension, path_to_phi4):
         # Loading the model:
         pipeline = transformers.pipeline(
             "text-generation",
-            model="{}".format(path_to_phi4),
+            model="{}".format(path_to_gemma3),
             model_kwargs={"torch_dtype": "auto"},
             device_map="auto",
+            max_new_tokens=1024
         )
 
-        # Using the model:
-        security_findings = []
         blocks = split_for_model(content_lines)  # Splitting the content to small blocks for the model
+        print(" === Analysis === ")
         for block in blocks:
-            temp_message = build_prompt(block, "file", extension, security_findings)  # Writing the prompt
+            temp_message = build_prompt(block, "file", extension)  # Writing the prompt
             temp_output = pipeline(temp_message)  # Using the model
-            security_findings.append(temp_output[0]["generated_text"])  # Saving the output
-
-        # Here, we finished to use the model to examine the file's blocks.
-        # Now, we use it again to summarize all the findings to final result:
-
-        security_findings_as_string = "\n".join(security_findings)
-        blocks = split_for_model(security_findings_as_string)  # For cases when the findings are too big for the model
-
-        print ("Start Analyzing:")
-        for block in blocks:
-            temp_message = build_prompt(block, "summarize", extension)
-            temp_output = pipeline(temp_message)
             print(temp_output[0]["generated_text"])
+
+        return None
+
+        #
+        #
+        # # Using the model:
+        # security_findings = []
+        # blocks = split_for_model(content_lines)  # Splitting the content to small blocks for the model
+        # for block in blocks:
+        #     temp_message = build_prompt(block, "file", extension, security_findings)  # Writing the prompt
+        #     temp_output = pipeline(temp_message)  # Using the model
+        #     security_findings.append(temp_output[0]["generated_text"])  # Saving the output
+        #
+        # # Here, we finished to use the model to examine the file's blocks.
+        # # Now, we use it again to summarize all the findings to final result:
+        # security_findings_as_string = "\n".join(security_findings)
+        # blocks = split_for_model(security_findings_as_string)  # For cases when the findings are too big for the model
+        #
+        # print ("Start Analyzing:")
+        # for block in blocks:
+        #     temp_message = build_prompt(block, "summarize", extension)
+        #     temp_output = pipeline(temp_message)
+        #     print(temp_output[0]["generated_text"])
 
     except Exception as error:
         message = "Using Model Error: {}.".format(error)
@@ -149,7 +162,7 @@ def main():
     # Checking the input arguments:
     length = len(sys.argv)
     if length not in [2,3]:
-        message = "Invalid Input Error: A valid input must contain a C/C++ file, and a path to local PHI4 (optional)."
+        message = "Invalid Input Error: A valid input must contain a C/C++ file, and a path to local gemma31b (optional)."
         failure_message(message)
 
     file = sys.argv[1]
@@ -161,16 +174,16 @@ def main():
     content_lines = extract_content_safely(file)
 
     if length == 3:
-        path_to_phi4 = sys.argv[2]
-        if not (os.path.exists(path_to_phi4)):  # Just validating the existence of the path, not its content
-            message = "Invalid Input Error: The PHI4' path is invalid or not exist."
+        path_to_gemma3 = sys.argv[2]
+        if not (os.path.exists(path_to_gemma3)):  # Just validating the existence of the path, not its content
+            message = "Invalid Input Error: The gemma3b1 path is invalid or not exist."
             failure_message(message)
 
     else:  # length == 2
-        print("Note: No path to a local PHI4 was provided. Default path will be used instead.")
-        path_to_phi4 = "default_PHI4_model"
+        print("Note: No path to a local gemma3:1b was provided. Default path will be used instead.")
+        path_to_gemma3 = "default_gemma31b_model"
 
-    using_model(content_lines,extension, path_to_phi4)
+    using_model(content_lines,extension, path_to_gemma3)
     sys.exit(0)
 
 
