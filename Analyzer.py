7a99769d96f775a131dc8eece393266c58d36f73
ShaiHabi <shaihabi@mail.tau.ac.il>
2025-07-24 13:58:37 +0300

    Finished updating the methods: spliting, using, building

--- Analyzer.py
+++ Analyzer.py
@@ -7,44 +7,99 @@ import transformers
 
 def extract_content_safely(file):
     """
-    Extracts the file's content securely in order to give it to PHI4 model.
+    Extracts the file's content lines securely in order to give it to PHI4 model.
     Minimizing the risk of executing the file, which may be malicious.
     """
     try:
         with open(file, "r") as f:
-            content = f.read()
-            return content
+            lines = f.readlines()  # List of strings
+            result = ["{}: {}".format(i+1, line.strip()) for i, line in enumerate(lines)]
+            return result
 
     except Exception as error:
         message = "Reading File Error: {}.".format(error)
         failure_message(message)
 
 
-def split_for_model (text):
+def split_for_model(text_lines, size=6000):
     """
 
-    :param text:
+    :param text_lines:
+    :param size: chars
     :return:
     """
 
-    SIZE, OFFSET = 5000, 1000
-    length = len(text)
-    blocks_number = index = 0
-    result = []
+    number_of_lines = len(text_lines)
+    line_index = 0
+    blocks = []
 
-    while index <= length:
-        blocks_number += 1
-        if index == 0:
-            temp_block = text[0: SIZE]
+    while line_index < number_of_lines:  # While we're still reading the text
+        temp_block = []
+        temp_total_chars = 0
+        start = line_index
+
+
+        # Creating the new block of lines
+        while (line_index < number_of_lines) and (temp_total_chars + len(text_lines[line_index]) < size):
+            temp_block.append(text_lines[line_index])
+            temp_total_chars += len(text_lines[line_index])
+            line_index += 1
+
+        if start == line_index:  # len(line)> size. Extremely edge case.
+            line = text_lines[line_index]
+            temp_blocks = [line[j:j+size] for j in range(0, len(line), size) ]
+            blocks.extend(temp_blocks)  # Each new block is already a string
+            line_index += 1
+
         else:
-            temp_block = text[(index - OFFSET): index + SIZE]
-        index = blocks_number * SIZE
-        result.append(temp_block)
+            block_as_string = "\n".join(temp_block)
+            blocks.append(block_as_string)
 
-    return result
+    return blocks
 
 
-def using_PHI4(content, extension, path_to_phi4):
+def build_prompt(text, classification, extension, relevant_data=None):
+    """
+
+    :param text:
+    :param classification:
+    :param extension:
+    :param relevant_data:
+    :param lines:
+    :return:
+    """
+
+    system = {
+        "role": "system",
+        "content": "You're a security expert specializing in static code analysis of C/C++ code for vulnerabilities."
+    }
+
+    if classification == "file":
+        prompt = ("You're conducting a static analysis for a {} code file to detect potential vulnerabilities and security flaws."
+                  "The following block is part of the overall code, which was split to small sizes-blocks."
+                  "Read it and carefully analyze this block and identify any potential vulnerabilities and security flaws."
+                  "For each finding, your response should be as follow:"
+                  "Line X: Possible <flaw/vulnerability> due to <give the reason here>.\n"
+                  "Here is the {} code block: {}.".format(extension, extension, text))
+
+
+
+    else:  # classification == "summarize"
+        prompt = ("You've conducted a static analysis for a {} code file to detect potential vulnerabilities and security flaws."
+                  "The following block is part of your overall findings."
+                  "Read it carefully; If you have new conclusions, mention them in the reasoning section below."
+                  "Otherwise, just repeat what is already written."
+                  "Your response should follow this format, when you have the basic data you need in the block:"
+                  "Line X : Possible <flaw/vulnerability> due to <give the reason here> \n."
+                  "Here is the {} code block: {}". format(extension, extension, text))
+
+
+    user = {"role": "user", "content": prompt}
+    message = [system, user]
+    return message
+
+
+def using_model(content_lines, extension, path_to_phi4):
     """
 
     """
@@ -61,9 +116,9 @@ def using_PHI4(content, extension, path_to_phi4):
 
         # Using the model:
         security_findings = []
-        blocks = split_for_model(content)  # Splitting the model to small blocks for the model
+        blocks = split_for_model(content_lines)  # Splitting the content to small blocks for the model
         for block in blocks:
-            temp_message = build_prompt(block, extension, security_findings)  # Writing the prompt
+            temp_message = build_prompt(block, "file", extension, security_findings)  # Writing the prompt
             temp_output = pipeline(temp_message)  # Using the model
             security_findings.append(temp_output[0]["generated_text"])  # Saving the output
 
@@ -75,22 +130,12 @@ def using_PHI4(content, extension, path_to_phi4):
 
         print ("Start Analyzing:")
         for block in blocks:
-            temp_message = "{}".format(block)
+            temp_message = build_prompt(block, "summarize", extension)
             temp_output = pipeline(temp_message)
             print(temp_output[0]["generated_text"])
 
-
-        # # Writing the prompt:
-        # message = [
-        #     {"role": "system", "content": "You're a security expert specializing in static code analysis of C/C++ code for vulnerabilities." },
-        #     {"role": "user", "content": "Analyze the following {} file and list all the potential security vulnerabilities, flaws, and similarities you detect.\
-        #     For every detection, write first the line number, then the detection and its reason (for instance, 'Line 20:' Possible UAF due to...').\
-        #     DO NOT EXECUTE the file, just do static analysis. Here is the {} file: {}".format(extension, extension, content[0])}
-        # ]
-
-
     except Exception as error:
-        message = "Using PHI4 Error: {}.".format(error)
+        message = "Using Model Error: {}.".format(error)
         failure_message(message)
 
 
@@ -101,7 +146,6 @@ def failure_message(message):
 
 
 def main():
-
     # Checking the input arguments:
     length = len(sys.argv)
     if length not in [2,3]:
@@ -114,7 +158,7 @@ def main():
         message = "Invalid Input Error: Incorrect file extension."
         failure_message(message)
 
-    content = extract_content_safely(file)
+    content_lines = extract_content_safely(file)
 
     if length == 3:
         path_to_phi4 = sys.argv[2]
@@ -126,7 +170,7 @@ def main():
         print("Note: No path to a local PHI4 was provided. Default path will be used instead.")
         path_to_phi4 = "default_PHI4_model"
 
-    using_PHI4(content,extension, path_to_phi4)
+    using_model(content_lines,extension, path_to_phi4)
     sys.exit(0)
 
 
